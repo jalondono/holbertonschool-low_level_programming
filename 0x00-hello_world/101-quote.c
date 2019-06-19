5e8cc28754d522a26bd79f3f556a03e8b1064d18
jalondono <juanlondono151776@hotmail.com>
2019-06-19 21:35:03 +0000

    Without Printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 int write(int filedes, const char *buf, unsigned int nbyte);
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 return (1);
 }
