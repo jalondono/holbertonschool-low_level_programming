76ed94709f320a2cbe6f2998468670209ae0d4c0
jalondono <juanlondono151776@hotmail.com>
2019-06-19 18:40:40 +0000

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-
 int main (void)
 {
-  puts ("Programming is like building a multilingual puzzle") ;
+  puts ("\"Programming is like building a multilingual puzzle") ;
   return(0);
 }
