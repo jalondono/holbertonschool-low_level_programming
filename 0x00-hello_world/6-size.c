ce8e948b6cd0ceaf1ea3fb1a4d4dd7fffc22e5ab
jalondono <juanlondono151776@hotmail.com>
2019-06-19 20:11:30 +0000

    printing size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,19 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
+
+int main(void)
+{
 char charType;
 int integerType;
 long int Lint;
 long long int Llint;
 float Float;
-
-int main(void)
-{
- 
-  printf("Size of a char: %ld byte(s)\n",sizeof(charType));
-  printf("Size of an int: %ld byte(s)\n",sizeof(integerType));
-  printf("Size of a long int: %ld byte(s)\n",sizeof(Lint));
-  printf("Size of a long long int: %ld byte(s)\n",sizeof(Llint));
-  printf("Size of float: %ld byte(s)\n",sizeof(Float));
-  return (0);
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(Lint));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(Llint));
+printf("Size of float: %ld byte(s)\n", sizeof(Float));
+return (0);
 }
