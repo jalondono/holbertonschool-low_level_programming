d77c68d2c3ffd3e1996eecf11a8c58f12d064689
jalondono <juanlondono151776@hotmail.com>
2019-06-19 20:32:05 +0000

    printing size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ long long int Llint;
 float Float;
 printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
-printf("Size of a long int: %ld byte(s)\n", sizeof(Lint));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(Llint));
 printf("Size of float: %ld byte(s)\n", sizeof(Float));
 return (0);
