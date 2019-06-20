01345de35549827291f9d332df17b16e7710b425
jalondono <juanlondono151776@hotmail.com>
2019-06-20 19:24:08 +0000

    uploading task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - prueba 6
+  * Return - 0
+  */
+int main(void)
+{
+char i = '0';
+for (i = '0'; i <= '9' ; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
