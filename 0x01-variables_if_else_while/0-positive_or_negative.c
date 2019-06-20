59cc269ffe7ab6960b634c69fe7ffada0cdb8e1c
jalondono <juanlondono151776@hotmail.com>
2019-06-20 18:17:22 +0000

    uploadind task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,18 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d ", n);
-printf("is positive \n");
+printf("%d is positive \n", n);
 }
 if (n == 0)
 {
-printf("%d ", n);
-printf("is zero \n");
+printf("%d is zero \n", n);
 }
 if (n < 0)
 {
-printf("%d ", n);
-printf("is negative \n");
+printf("%d is negative \n", n);
 }
 return (0);
 }
