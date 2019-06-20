9208aab043ad5cc8548c6097656c4ad72c712900
jalondono <juanlondono151776@hotmail.com>
2019-06-20 18:28:23 +0000

    uploadind task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 if (m > 5)
 {
-printf("Last digit of %d and is greater than 5\n", n, m);
+printf("Last digit of %d is %d and is greater than 5\n", n, m);
 }
 if (m == 0)
 {
