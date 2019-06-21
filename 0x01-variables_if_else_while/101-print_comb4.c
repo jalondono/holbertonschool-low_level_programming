31bc1305e723e389852a92d702acce1af0a7d762
jalondono <juanlondono151776@hotmail.com>
2019-06-21 04:08:18 +0000

    101 Advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - prueba 10
+  * Return: Always 0
+  */
+int main(void)
+{
+int a = 0;
+int aprima;
+int b;
+int bprima;
+int c;
+int cprima;
+for (a = 0; a <= 9; a++)
+{
+aprima = a + '0';
+for (b = 0; b <= 9 ; b++)
+{
+bprima = b + '0';
+for (c= 0 ; c <= 9; c++)
+{
+cprima = c +'0';
+if (a == b || b == c || a > b || b > c)
+{
+}
+else
+{
+putchar(aprima);
+putchar(bprima);
+putchar(cprima);
+if (a == 7 && b == 8 && c == 9)
+{
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
