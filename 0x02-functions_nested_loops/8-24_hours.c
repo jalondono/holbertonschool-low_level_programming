ada65999da45d830f31a3c4119b170529d3e174f
jalondono <juanlondono151776@hotmail.com>
2019-06-25 04:52:31 +0000

    24/7

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+int i;
+int j;
+int hour;
+int hourp;
+int min;
+int minp;
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+hourp = i / 10;
+hour = i % 10;
+minp = j / 10;
+min = j % 10;
+_putchar(hourp + '0');
+_putchar(hour + '0');
+_putchar(':');
+_putchar(minp + '0');
+_putchar(min + '0');
+_putchar('\n');
+}
+}
+}
