09143d7f4c840ed9fc4c82d9740e80302347ee98
jalondono <juanlondono151776@hotmail.com>
2019-06-25 04:45:07 +0000

    printing Holberton from a function

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+int tamano;
+char cadena[]="Holberton";
+tamano =  sizeof(cadena)/sizeof(char);
+for(i=0;i<tamano;i++){
+_putchar(cadena[i]);
+}
+_putchar('\n');
+return (0);
+}
