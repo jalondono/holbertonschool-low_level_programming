1f704f07b4c068c7ab93d54d64b262c45ae3da60
jalondono <juanlondono151776@hotmail.com>
2019-06-25 04:46:46 +0000

    printing the alphabet from a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+ /**
+ * Second task for holberton
+ * Return: 0
+ */
+void  print_alphabet(void)
+{
+char letras='a';
+for (letras = 'a'; letras <= 'z'; letras++)
+{
+_putchar(letras); 
+}
+_putchar('\n');
+}
