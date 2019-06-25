2ac560f72c914a45ea71dbe214568ecc1c6ff6ae
jalondono <juanlondono151776@hotmail.com>
2019-06-25 15:56:20 +0000

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,16 +6,16 @@
  */
 int print_last_digit(int a)
 {
+a = a % 10;
 if (a < 0)
 {
 a = a * -1;
-a = a % 10;
 _putchar(a + '0');
+return (a);
 }
 else
 {
-a = a % 10;
 _putchar(a + '0');
-}
 return (a);
+}
 }
