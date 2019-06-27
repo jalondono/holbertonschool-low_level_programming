85147e733ee264792011a5fe99be0a7d31539269
jalondono <juanlondono151776@hotmail.com>
2019-06-27 07:45:47 -0500

    0 task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _isupper - main principal
+ * @c : variable de entrada
+ * Return : devuelve un entero
+ */
+
+int _isupper(int c)
+{
+int  a;
+if(c >= 65 && c <= 90)
+{
+a = 1;
+}
+else
+{
+a = 0;   
+}
+ return (a);
+}
