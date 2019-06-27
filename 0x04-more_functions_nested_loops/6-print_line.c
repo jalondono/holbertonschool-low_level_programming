c66b1c92d0a3ddfd814cd237e6aef334cabe4678
jalondono <juanlondono151776@hotmail.com>
2019-06-27 10:35:29 -0500

    Upload

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*print_line - programs things
+*Result: always returns 0
+*@n: numero de iteraciones
+*Return: value depending on function
+*/
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+_putchar('_');
+}
+_putchar('\n');
+}
