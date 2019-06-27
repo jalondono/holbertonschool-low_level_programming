518b4aa186f0e16fbf553b6010d4b69ce381263a
jalondono <juanlondono151776@hotmail.com>
2019-06-27 08:30:29 -0500

    upload task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_isdigit - program thisgs
+ *Result: always return 0
+ *@c
+ *Return: value depending on function
+ */
+
+int _isdigit(int c)
+{
+int a;
+if(c >= 48 && c <= 57)
+{
+a = 1;
+}
+else
+{
+a = 0;
+}
+return (a);
+}
