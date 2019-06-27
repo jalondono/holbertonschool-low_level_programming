913533a10f5a7de902098a97b55b23dfdd91ca9f
jalondono <juanlondono151776@hotmail.com>
2019-06-27 12:35:13 -0500

    upload task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+*main - programs things
+*Result: always returns 0
+*Return: value depending on function
+*/
+
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+if (i == 100)
+{
+printf("Buzz\n");
+}
+else
+{
+printf("Buzz ");
+}
+}
+else if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+}
