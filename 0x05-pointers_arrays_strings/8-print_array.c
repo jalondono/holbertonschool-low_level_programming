50c4404e09de75b5aa1ab3efcce69828639fb714
jalondono <juanlondono151776@hotmail.com>
2019-07-02 20:38:00 +0000

    upload task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - program thisgs
+ *Result: always return 0
+ *@a: puntero de array
+ *@n:variable numero de iteraciones
+ *Return: value depending on function
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n; i++)
+	{
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
