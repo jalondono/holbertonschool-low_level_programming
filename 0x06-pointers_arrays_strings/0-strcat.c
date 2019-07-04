fe7afe51faf1fe5b9ff96908438cf51bd4538602
jalondono <juanlondono151776@hotmail.com>
2019-07-04 17:56:46 +0000

    upload task 0 - 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*_strcat - program thisgs
+*Result: always return 0
+*@dest: variable de entrada
+*@src: entrada source
+*Return: value depending on function
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*_strncat - program thisgs
+*Result: always return 0
+*@dest: variable de entrada
+*@src: entrada source
+*@n: numbers of characters
+*Return: value depending on function
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest [i + j] = '\n';
+	return (dest);
+}
