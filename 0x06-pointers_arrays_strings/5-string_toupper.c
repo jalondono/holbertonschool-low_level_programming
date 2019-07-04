28053b039efde93369cf9f50f3eb0c7bb3774d85
jalondono <juanlondono151776@hotmail.com>
2019-07-04 21:32:19 +0000

    uploading task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,9 +7,20 @@
  */
 char *string_toupper(char *s)
 {
+	int i = 0;
 	while (s[i] != '\0')
 	{
-		s[i] = s[i] - 32;
+		if(s[i] >= 65 && s[i] <= 90)
+		{
+		}
+		else if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		else
+		{
+		}
+		i++;
 	}
 	return (s);
 }
