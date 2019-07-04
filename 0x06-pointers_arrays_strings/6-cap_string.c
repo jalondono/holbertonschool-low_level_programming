b1c9a451aef26c027e5ca1e65f998a6ab9f4a901
jalondono <juanlondono151776@hotmail.com>
2019-07-04 22:51:23 +0000

    upload task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ *cap_string - program thisgs
+ *Result: always return 0
+ *@s: variable de entrada
+ *Return: value depending on function
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 35 && s[i] <= 44) || (s[i] == 59) || s[i] == 46)
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		if (s[i] == 123 || s[i] == 125 || s[i] == '\n')
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+		if (s[i] == '\t' || s[i] == 32 || s[i] == 33)
+		{
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+			i++;
+	}
+	return (s);
+}
