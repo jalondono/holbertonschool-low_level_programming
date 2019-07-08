625426f90e3d4be2a842653af2281c44aeeee4d1
jalondono <juanlondono151776@hotmail.com>
2019-07-08 17:02:06 +0000

    upload task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_memcpy - program thisgs
+ *Result: always return 0
+ *@dest: variable de entrada
+ *@src: variable entrada
+ *@n: variable entrada
+ *Return: value depending on function
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(dest + i) = src[i];
+	}
+}
