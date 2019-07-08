f3b4934838b2cd925ec454a5b817683a95f0a63f
jalondono <juanlondono151776@hotmail.com>
2019-07-08 19:21:24 +0000

    upload task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strchr - program thisgs
+ *Result: always return 0
+ *@s: variable de entrada
+ *@c: entrada source
+ *Return: value depending on function
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			break;
+		}
+		s++;
+	}
+	return (s);
+}
