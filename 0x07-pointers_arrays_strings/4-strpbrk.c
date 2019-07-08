8845f57e3c50ca1b1577028bbd210c94202de3ff
jalondono <juanlondono151776@hotmail.com>
2019-07-08 21:28:44 +0000

    upload task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *_strpbrk - program thisgs
+ *Result: always return 0
+ *@s: variable de entrada
+ *@accept: entrada source
+ *Return: value depending on function
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int c = 1;
+	int h = 0;
+	char k = 0;
+
+	while (*s != '\0'  && c == 1)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				c = 0;
+				break;
+			}
+		}
+		if (c == 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (s);
+}
