2773790cb521b1e3a2810e8164806878247810fd
jalondono <juanlondono151776@hotmail.com>
2019-07-09 04:22:51 +0000

    upload task 9

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ *_memcpy - program thisgs
+ *Result: always return 0
+ *@dest: variable de entrada
+ *@src: variable entrada
+ *@n: variable entrada
+ *Return: value depending on function
+ */
---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *print_chessboard - program thisgs
+ *Result: always return 0
+ *@a: Array
+ *Return: value depending on function
+ */
+void print_chessboard(char (*a)[8])
+{
+
+}
