6e01690b0f7e99cd7a5f719fe5032db3e2948408
jalondono <juanlondono151776@hotmail.com>
2019-07-09 04:03:11 +0000

    upload task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,27 +7,28 @@
  *Return: value depending on function
  */
 unsigned int _strspn(char *s, char *accept)
+
 {
 	int i = 0;
 	int j = 0;
 	int k = 0;
-	int pesostring = 0;
-	int match = 0;
+	unsigned int match = 0;
 
-	while (accept[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		i++;
-	}
-	pesostring = i / sizeof(char);
-	for (j = 0; j <= pesostring; j++)
-	{
-		for (k = 0; k < pesostring; k++)
+		j = 0;
+		for (k = 0; accept[k] != '\0'; k++)
 		{
-			if (s[j] == accept[k])
+			if (s[i] == accept[k])
 			{
-				match++;
+				j = 1;
 			}
 		}
+		if (j == 0)
+		{
+			break;
+		}
+		match++;
 	}
 	return (match);
 }
