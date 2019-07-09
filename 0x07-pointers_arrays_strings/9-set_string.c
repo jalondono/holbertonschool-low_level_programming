d8d362c1b64710d4d0b1bb57a89cd09c9512e70e
jalondono <juanlondono151776@hotmail.com>
2019-07-09 04:20:46 +0000

    upload task 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *set_string - program thisgs
+ *Result: always return 0
+ *@s: variable de entrada
+ *@to: variable entrada
+  *Return: value depending on function
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
