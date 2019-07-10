d4e39ea514d229032f06d237806003b8a681dad7
jalondono <juanlondono151776@hotmail.com>
2019-07-10 03:28:21 +0000

    upload task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,33 @@
 #include "holberton.h"
 /**
- *_memcpy - program thisgs
+ *_strstr - find and string pattern inside of src, and return the firs positio
  *Result: always return 0
- *@dest: variable de entrada
- *@src: variable entrada
- *@n: variable entrada
- *Return: value depending on function
+ *@haystack: string SRC
+ *@needle: string pattenr
+ *Return: a pointer
  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	while (haystack[i])
+	{
+		if (haystack[i] == needle[j])
+		{
+			j++;
+			if (needle[j] == '\0')
+			{
+				return ((haystack + i) - (j - 1));
+			}
+				else
+				{
+					j = 0;
+				}
+		}
+		i++;
+	}
+	if (*needle == '\0')
+		return (haystack);
+	return (0);
+}
