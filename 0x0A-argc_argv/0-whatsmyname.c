6438497e60d708880ceaa6175331ec8c0000e3b9
jalondono <juanlondono151776@hotmail.com>
2019-07-12 20:05:22 +0000

    upload task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Print an string using recursion
+ *@argc: number of arguments
+ *@argv: vector with the values
+ *Result: always return 0
+ *Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	printf("%s\n", argv[0]);
+}
