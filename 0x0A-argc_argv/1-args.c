52f08487a3019504da7f0f44a6563520fc6a59eb
jalondono <juanlondono151776@hotmail.com>
2019-07-12 20:29:36 +0000

    upload task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Print an string using recursion
+ *@argc: number of arguments
+ *@argv: vector with the values
+ *Result: always return 0
+ *Return: nothing
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	int i = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 0; i < argc; i++)
+		{
+		}
+	}
+	printf("%d\n", i - 1);
+}
