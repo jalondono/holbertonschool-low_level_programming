a596190a480a3037647a72fff0951113b1e9d11c
jalondono <juanlondono151776@hotmail.com>
2019-07-16 20:03:24 +0000

    upload task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *_strdup - duplicate ans string in a new buffer
+ *@str: string to copy on new buffer
+ *Result: a matrix with H character
+ *Return: a pointer if can do  the buffer
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *value;
+
+	if (str[0] == '\0')
+		return ('\0');
+	value = malloc(sizeof(char) * i);
+	if (value == 0)
+		return ('\0');
+	while (str[i])
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		value[j] = str[j];
+	}
+	return (value);
+}
