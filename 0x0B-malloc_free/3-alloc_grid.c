a48a42fc0b88a0811cfce150a2fe5fb036c0e2cd
jalondono <juanlondono151776@hotmail.com>
2019-07-16 23:19:58 +0000

    upload task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *alloc_grid - duplicate ans string in a new buffer
+ *@width: string to copy on new buffer
+ *@height: height of string
+ *Result: a matrix with H character
+ *Return: a pointer if can do  the buffer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **value;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	value = malloc(width * sizeof(int *));
+	for (i = 0; i < width; i++)
+		value[i] = malloc(height * sizeof(int));
+
+	for (j = 0; j < width; j++)
+	{
+		for (k = 0; k < height; k++)
+		{
+			value[j][k] = 0;
+		}
+	}
+	return (value);
+}
