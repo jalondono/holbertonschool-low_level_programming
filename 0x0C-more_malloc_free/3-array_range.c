9b74d3e31df87c685e7b155b1ed2dcacf21957c9
jalondono <juanlondono151776@hotmail.com>
2019-07-18 21:07:27 +0000

    upload task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *array_range - generate an array with values in a range gave it
+ *@min: min range
+ *@max: max range
+ *Result: a matrix with H character
+ *Return: a pointer if can do  the buffer
+ */
+int *array_range(int min, int max)
+{
+	int *var = NULL;
+	int i = 0;
+
+	if (min < 0 || max < 0)
+		return (NULL);
+
+	if (min > max)
+		return (NULL);
+	var = (int *)malloc(sizeof(int) * (max - min) + 1);
+	if (var == NULL)
+		return (NULL);
+	for (i = min; i <= (max - min); i++)
+	{
+		var[i] = i;
+	}
+	return (var);
+}
