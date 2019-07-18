c48114f0a3c94839f47de881a9e5a0f6ea7979b6
jalondono <juanlondono151776@hotmail.com>
2019-07-18 18:25:19 +0000

    upload task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- *argstostr - duplicate ans string in a new buffer
- *@ac: string to copy on new buffer
- *@av: height of string
+ *malloc_checked - duplicate ans string in a new buffer
+ *@b: string to copy on new buffer
  *Result: a matrix with H character
  *Return: a pointer if can do  the buffer
  */
 void *malloc_checked(unsigned int b)
 {
 	int *var = NULL;
-	var = malloc(sizeof(int) * b);
+
+	var = malloc(b);
 	if (var == NULL)
 		exit(98);
 	return (var);
