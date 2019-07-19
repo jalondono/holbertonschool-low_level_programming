09ba00a74355cc021deb1853acbf5e14a562c6a1
jalondono <juanlondono151776@hotmail.com>
2019-07-19 19:04:43 +0000

    upload task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,9 @@
  */
 int *array_range(int min, int max)
 {
-	int *var = NULL;
+	int *var;
 	int i = 0;
 	int j = 0;
-
-
-	if (min < 0 || max < 0)
-		return (NULL);
 
 	if (min > max)
 		return (NULL);
@@ -25,7 +21,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	for (i = 0, j = min; i <= (max - min); i++, j++)
 	{
-		var[i] = j;
+		*(var + i) = j;
 	}
 	return (var);
 }
