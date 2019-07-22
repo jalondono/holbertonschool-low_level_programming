f3e4235bfac6da46b0017ea04c94355baef51d29
jalondono <juanlondono151776@hotmail.com>
2019-07-22 20:02:28 +0000

    upload  task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
