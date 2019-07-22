8d3e39e6ff66ae6bc04c687ed4807fb711b9db39
jalondono <juanlondono151776@hotmail.com>
2019-07-22 22:19:55 +0000

    upload task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *free_dog - program thisgs
+ *Result: always return 0
+ *@d: structura de datos
+ *Return: value depending on function
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner;)
+}
