321f68ba0994822f9512e39328ba4a5284004253
jalondono <juanlondono151776@hotmail.com>
2019-07-24 21:48:52 +0000

    upload task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *int_index - print a name throught a pointer
+ *@array: string to copy on new buffer
+ *@size: pointer to function
+ *@cmp: puntero a la funcion
+ *Result: a matrix with H character
+ *Return: a pointer if can do  the buffer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int a = 0;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+	for (i = 0; i < size; i++)
+	{
+		a = cmp(array[i]);
+		if (a == 1)
+			return (i);
+		if (i == size - 1)
+			return (-1);
+	}
+	return (i);
+}
