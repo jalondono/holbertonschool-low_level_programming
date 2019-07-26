c34e32a208d1c8c9560cb5e3ca5bbb5d1ed833e0
jalondono <juanlondono151776@hotmail.com>
2019-07-26 15:30:39 +0000

    upload task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - sum all argv in the function
+ *@n: string to copy on new buffer
+ *Result: a matrix with H character
+ *Return: an int with the result of sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int x = 0;
+
+	va_start (args, n);
+	for (x = 0; x < n; x++)
+		sum = sum + va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef _sum_them_all_h_
+#define _sum_them_all_h_
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
