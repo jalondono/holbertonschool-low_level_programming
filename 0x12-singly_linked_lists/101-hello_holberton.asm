b1c292255872c97b194e745c603cf2034ff01891
jalondono <juanlondono151776@hotmail.com>
2019-08-01 23:12:48 +0000

    upload task advanced 2

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+section   .data
+message:	  db        "Hello, Holberton", 10 ; note the newline at the end
+.size equ $ - message
+	global    main
+
+	          section   .text
+main:
+	          mov       rdi, 1 ; file handle 1 is stdout
+	          mov       rsi, message ; address of string to output
+	          mov       rdx, message.size	 ; number of bytes
+		mov rax,1
+		syscall		 ; invoke operating system to do the write
+	          mov       rax, 60	 ; system call for exit
+	          xor       rdi, 0	 ; exit code 0
+	          syscall		 ; invoke operating system to exit
