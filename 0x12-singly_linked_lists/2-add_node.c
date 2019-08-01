21f62995147bb1208daa78f8cfe8274178e569b2
jalondono <juanlondono151776@hotmail.com>
2019-08-01 20:51:40 +0000

    upload first time task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+/**
+ *_strlen - program thisgs
+ *Result: always return 0
+ *@s: variable de entrada
+ *Return: value depending on function
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
+
+/**
+ * and_node - singly linked list
+ * @h: structure
+ * Description: singly linked list node structure
+ *Return: a number of elements
+ * for Holberton project
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int len = 0;
+	char * dubstring;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	dubstring = strdup(str);
+	len = _strlen(dubstring);
+	new->str = dubstring;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (*head);
+
+
+
+
+}
