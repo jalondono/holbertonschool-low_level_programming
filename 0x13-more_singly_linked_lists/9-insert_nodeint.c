e7ed481ee6d4ab27445ae98ff834e9b1eab47941
jalondono <juanlondono151776@hotmail.com>
2019-08-06 17:46:06 +0000

    upload task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -45,7 +45,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new = add_nodeint(head, n);
 		return (new);
 	}
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
