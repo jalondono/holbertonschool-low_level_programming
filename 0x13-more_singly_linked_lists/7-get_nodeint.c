55123cf2b470be2c01cab432c4f08557d4a87a18
jalondono <juanlondono151776@hotmail.com>
2019-08-06 14:49:53 +0000

    Put a condition to return NULL when the head is NULL

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *current = head;
-
+	if (head == NULL)
+		return (NULL);
 	for (i = 0; i < index; i++)
 	{
 		current = current->next;
