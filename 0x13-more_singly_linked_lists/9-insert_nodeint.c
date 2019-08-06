3c1f527c00af17a950c8b9d786eb30761523a336
jalondono <juanlondono151776@hotmail.com>
2019-08-06 16:02:18 +0000

    upload task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ *insert_nodeint_at_index - program thisgs
+ *Result: always return 0
+ *@head: pointer to structure
+ *@idx: index to add a node
+ *@n: value to node
+ *Return: value depending on function
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *copy = *head;
+	listint_t *new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	while (i < idx - 1)
+	{
+		copy = copy->next;
+		if (copy == NULL)
+			return (NULL);
+		i++;
+	}
+	new->n = n;
+	new->next = copy->next;
+	copy->next = new;
+	return (new);
+}
