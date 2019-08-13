8769be94d7e3e7a59643baf056bb194c4d961ba0
jalondono <juanlondono151776@hotmail.com>
2019-08-13 21:12:20 +0000

    upload task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,10 +24,13 @@ int _strlen(char *s)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd = 0;
 	int result;
 
 	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
@@ -36,9 +39,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		else
 			return (1);
 	}
-	fd = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
-		return (-1);
 	result = write(fd, text_content, _strlen(text_content));
 	if (result == -1)
 	{
