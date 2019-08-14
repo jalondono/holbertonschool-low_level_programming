1f31a45974f9eb341e406d8ab4ea4b0339b7475f
jalondono <juanlondono151776@hotmail.com>
2019-08-14 01:02:03 +0000

    upload task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,29 +9,28 @@
 int main(int argc, char *argv[])
 {
 	int resultfrom = 0, resultto = 0, fdfrom, fdto, closefrom, closeto = 0;
-	char *namefrom, *nameto;
 	char buf[1024];
 
 	if (argc != 3)
 	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	} namefrom = argv[1], nameto = argv[2];
-	fdfrom = open(namefrom, O_RDONLY);
+	}
+	fdfrom = open(argv[1], O_RDONLY);
 	if (fdfrom == -1)
-	{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", namefrom);
+	{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	} fdto = open(nameto, O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	} fdto = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fdto == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", nameto);
+	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
 	while ((resultfrom = read(fdfrom, buf, 1024)) != 0)
 	{
 		if (resultfrom == -1)
-		{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", namefrom);
+		{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", argv[1]);
 			exit(98); }
 		resultto = write(fdto, buf, resultfrom);
 		if (resultto  == -1)
-		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", nameto);
+		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
