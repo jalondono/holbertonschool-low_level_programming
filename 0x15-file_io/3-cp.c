b1cd12bb2beb31c9adac499cf64f9c6cddd64c72
jalondono <juanlondono151776@hotmail.com>
2019-08-14 00:26:25 +0000

    upload task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+/**
+ *main - program thisgs
+ *Result: always return 0
+ *@argc: number of arguments
+ *@argv: arguments
+ *Return: value depending on function
+ */
+int main(int argc, char *argv[])
+{
+	int resultfrom, resultto, fdfrom, fdto = 0;
+	char *namefrom;
+	char *nameto;
+	int closefrom, closeto;
+	char buf[1024];
+
+	if (argc != 3)
+	{dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	} namefrom = argv[1], nameto = argv[2];
+
+	fdfrom = open(namefrom, O_RDWR);
+	if (fdfrom == -1)
+	{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", namefrom);
+		exit(98);
+	} fdto = open(nameto, O_CREAT | O_TRUNC | O_RDWR, 0664);
+	if (fdto == 0)
+	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", nameto);
+		exit(99);
+	} resultfrom = read(fdfrom, buf, 1024);
+	if (resultfrom == -1)
+	{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", namefrom);
+		exit(98);
+	} while (resultfrom != 0)
+	{
+		resultto = write(fdto, buf, resultfrom);
+		if (resultto  == -1)
+		{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", nameto);
+			exit(99);
+		} resultfrom = read(fdfrom, buf, 1024);
+		if (resultfrom == -1)
+		{dprintf(STDERR_FILENO, "Usage: Error: Can't read from file %s\n", namefrom);
+			exit(98);
+		}
+	}
+	closefrom = close(fdfrom);
+	closeto = close(fdto);
+	if (closefrom == -1 || closeto == -1)
+		exit(100);
+	return (0);
+}
