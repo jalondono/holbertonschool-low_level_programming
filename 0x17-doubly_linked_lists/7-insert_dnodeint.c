86cbadac9956cabeb80512fc82ef19a731c50290
jalondono <juanlondono151776@hotmail.com>
2019-09-14 19:55:32 +0000

    upload task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,31 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
-
-/**
- * add_dnodeint - singly linked list
- * @head: structure
- * @n:value
- * Description: singly linked list node structure
- *Return: a number of elements
- * for Holberton project
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	new->prev = NULL;
-	return (new);
-
-}
-
-
 /**
  *insert_nodeint_at_index - program thisgs
  *Result: always return 0
