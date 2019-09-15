cbde30372c40f7cb2de0c3f4b3347184916536c6
jalondono <juanlondono151776@hotmail.com>
2019-09-15 17:07:52 +0000

    upload segmentation fault at thre end

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,29 +1,5 @@
 #include "lists.h"
 /**
- *pop_dlistint - program thisgs
- *Result: always return 0
- *@head: pointer to structure
- *Return: value depending on function
- */
-int pop_dlistint(dlistint_t **head)
-{
-	dlistint_t *temp = NULL;
-
-	if (*head == NULL)
-		return (-1);
-	temp = (*head)->next;
-	if (temp != NULL)
-	{
-		temp->prev = NULL;
-		free(*head);
-		*head = temp;
-	}
-	else
-		*head = NULL;
-	return (1);
-	}
-
-/**
  * delete_dnodeint_at_index - singly linked list
  * @head: structure
  * @index:value
@@ -37,20 +13,28 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *maincopy = *head;
 	dlistint_t *tempnode;
 
-	while (maincopy != NULL || head != NULL)
+	if (head == NULL || *head == NULL)
+		return (-1);
+	while (maincopy)
 	{
 		if (i == index - 1)
 		{
 			tempnode = maincopy->next->next;
 			if (tempnode != NULL)
-				tempnode->prev = maincopy;
+				maincopy->next->next->prev = maincopy;
 			free(maincopy->next);
 			maincopy->next = tempnode;
 			return (1);
 		}
 		else if (index == 0)
 		{
-			return (pop_dlistint(head));
+			*head = (*head)->next;
+			if (maincopy->next != NULL)
+				(*head)->prev = NULL;
+			else
+				*head = NULL;
+			free(maincopy);
+			return (1);
 		}
 		maincopy = maincopy->next;
 		i++;
