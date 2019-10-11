d5179458af7e0b43ac6a2e1ac0088e1e1f2e9433
jalondono <juanlondono151776@hotmail.com>
2019-10-11 13:18:35 -0500

    added malloc to nodes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,25 @@
-//
-// Created by ja-holberton on 10/11/19.
-//
+#include "hash_tables.h"
+/**
+ * hash_table_create - allocate memory to hashtable
+ *@size: size of memory allocation
+ * Return: a pointer to memory allocation
+ */
 
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable = NULL;
+
+	hashtable = malloc(sizeof(struct hash_table_s) * 1);
+	if (hashtable == NULL)
+		return (NULL);
+	hashtable->array = malloc(sizeof(hash_table_t) * size);
+	if (hashtable->array == NULL)
+	{
+		free(hashtable);
+		return (NULL);
+	}
+	return (hashtable);
+
+
+
+}
