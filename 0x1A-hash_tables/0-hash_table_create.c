7ae2bc304764ff1754258fe036f4c1a4c2c9a0d2
jalondono <juanlondono151776@hotmail.com>
2019-10-11 16:16:06 -0500

    changed iter variable with i++ instead ++i

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,6 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hashtable = malloc(sizeof(hash_table_t) * 1);
 	if (hashtable == NULL)
 		return (NULL);
+	hashtable->size = size;
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashtable->array == NULL)
 	{
