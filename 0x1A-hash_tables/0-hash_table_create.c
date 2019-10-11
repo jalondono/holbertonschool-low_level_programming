e5c08ac142842c04fe824102f57ef5ae164fa8e1
jalondono <juanlondono151776@hotmail.com>
2019-10-11 13:30:46 -0500

    added malloc to nodes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,17 +8,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashtable = NULL;
+	int i = 0;
 
 	if (size == 0)
 		return (NULL);
 	hashtable = malloc(sizeof(struct hash_table_s) * 1);
 	if (hashtable == NULL)
 		return (NULL);
-	hashtable->array = malloc(sizeof(hash_node_t) * size);
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (hashtable->array == NULL)
 	{
 		free(hashtable);
 		return (NULL);
 	}
+	for (i = 0; i < 1024; ++i)
+		hashtable->array[i] = NULL;
 	return (hashtable);
 }
