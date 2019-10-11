e9d16b1ebeabfe0b5b3db8b7b32e1695d7b7836d
jalondono <juanlondono151776@hotmail.com>
2019-10-11 13:49:16 -0500

    upload task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,13 @@
-//
-// Created by ja-holberton on 10/11/19.
-//
+#include "hash_tables.h"
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
 
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
