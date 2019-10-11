57332c76b06c71f2ca45ab20fea63a583fca431a
jalondono <juanlondono151776@hotmail.com>
2019-10-11 17:39:23 -0500

    upload task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL)
 		return (0);
-	if (strcmp(key,"") == 0 || value == NULL)
+	if (strcmp(key, "") == 0 || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	newnode = malloc(sizeof(hash_node_t));
