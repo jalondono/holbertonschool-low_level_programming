abc6d2b9ec72088389616afbd5b6cf70fd946cdb
jalondono <juanlondono151776@hotmail.com>
2019-10-11 17:04:30 -0500

    upload task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,37 @@
-//
-// Created by ja-holberton on 10/11/19.
-//
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_set: function to add a item in a hash table
+ * @ht: hash table
+ *@key: key
+ * @value: value to storage
+ * Return: 1 if all went good
+ */
 
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *newnode;
+
+	if (ht == NULL)
+		return (0);
+	if  (key == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+		return (0);
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	newnode->next = NULL;
+	if (ht->array[index] == NULL)
+		ht->array[index] = newnode;
+	else
+	{
+		newnode->next = ht->array[index];
+		ht->array[index]->next = NULL;
+		ht->array[index] = newnode;
+	}
+	return (1);
+}
