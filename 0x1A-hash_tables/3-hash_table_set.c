46f54bf7bdb5392e9ef08d201e4f0f500f256df5
jalondono <juanlondono151776@hotmail.com>
2019-10-11 18:23:15 -0500

    upload task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,10 +30,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = newnode;
 		return (1);
 	}
-	else
-	{
-		tempnode = ht->array[index];
-		while (tempnode != NULL)
+	tempnode = ht->array[index];
+
+	while (tempnode != NULL)
 		{
 			if (strcmp(tempnode->key, key) == 0)
 			{
@@ -43,7 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			}
 			tempnode = tempnode->next;
 		}
-	}
+
 	newnode->next = ht->array[index];
 	ht->array[index] = newnode;
 	return (1);
