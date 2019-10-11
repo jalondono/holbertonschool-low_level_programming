0916adc549876ff1623eb7ce109998e3b84d0c4b
jalondono <juanlondono151776@hotmail.com>
2019-10-11 18:02:45 -0500

    upload task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -44,7 +44,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			tempnode = tempnode->next;
 		}
 		newnode->next = ht->array[index];
-		ht->array[index]->next = NULL;
 		ht->array[index] = newnode;
 	}
 	return (1);
