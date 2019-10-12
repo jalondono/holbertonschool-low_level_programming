e094bcb5fc837a3bd222fe8a9c6a819005eed8ad
jalondono <juanlondono151776@hotmail.com>
2019-10-11 23:39:08 -0500

    upload task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,35 @@
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
+
 /**
-* hash_table_delete - deletes a hash
-*@ht: where the hash is.
-*/
+ * hash_table_delete - deletes a hash table.
+ *
+ * @ht: The hash table
+ */
+
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp;
-	hash_node_t *delete;
-	unsigned long  int index_array;
-
-	if (ht)
+	unsigned int index = 0;
+	hash_node_t *node = NULL;
+	hash_node_t *aux = NULL;
+	
+	for (index = 0; index < ht->size; index++)
 	{
-		for (index_array = 0; index_array < ht->size; index_array++)
+		if (ht->array[index] != NULL)
 		{
-			tmp = ht->array[index_array];
-			while (tmp)
+			node = ht->array[index];
+			while (node)
 			{
-				delete = tmp;
-				free(delete->value);
-				free(delete->key);
-				free(delete);
-				tmp = tmp->next;
+				aux = node;
+				node = node->next;
+				free(aux->value);
+				free(aux->key);
+				free(aux);
 			}
-			free(tmp);
 		}
-		free(ht->array);
-		free(ht);
 	}
+	free(ht->array);
+	free(ht);
 }
