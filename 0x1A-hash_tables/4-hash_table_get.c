954c6969e840bec2a6fd7e4fa64de9ac2d7cace8
jalondono <juanlondono151776@hotmail.com>
2019-10-11 21:38:43 -0500

    upload task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - hash function
+ * @ht: size of
+ *@key: key
+ * Return: a randomly number of index
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index = 0;
+	hash_node_t *node;
+
+	if (ht == NULL || strcmp("", key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (NULL);
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
