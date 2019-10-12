9f6b2de572f4f4ac796fb21cdbd5a2dbcb0cc1ec
jalondono <juanlondono151776@hotmail.com>
2019-10-11 22:59:41 -0500

    upload task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,33 +1,31 @@
 #include "hash_tables.h"
 /**
-* hash_table_print - print hashes
-* @ht: the hash table
-*/
+ * hash_table_print - print a hash table
+ * @ht: size of
+ * Return: Nothing
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *tmp;
-	unsigned long cont = 0, index_array;
+	unsigned int i = 0;
+	hash_node_t *Tempnode;
+	int count = 0;
 
-	if (ht)
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i  = 0; i < ht->size; i++)
 	{
-		printf("{");
-		for (index_array = 0; index_array < ht->size;
-		     index_array++)
+		Tempnode = ht->array[i];
+		if (Tempnode == NULL)
+			continue;
+		while (Tempnode != NULL)
 		{
-			tmp = ht->array[index_array];
-			if (!tmp)
-			{
-				continue;
-			}
-			while (tmp)
-			{
-				if (cont != 0)
-					printf(", ");
-				printf("'%s': '%s'", tmp->key, tmp->value);
-				tmp = tmp->next;
-				cont++;
-			}
+			if (count != 0)
+				printf(", ");
+			printf("'%s': '%s'", Tempnode->key, Tempnode->value);
+			Tempnode = Tempnode->next;
+			count++;
 		}
-		printf("}\n");
 	}
+	printf("}\n");
 }
