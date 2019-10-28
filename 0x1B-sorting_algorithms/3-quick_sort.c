e0b8548aae78431299a87168343e0eb9e76bb9e2
jalondono <juanlondono151776@hotmail.com>
2019-10-28 15:39:43 -0500

    Quick sort and styles

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,68 @@
+#include "sort.h"
+
+void quicksort(int *array, int lo, int high, size_t size);
+int partition(int *array, int lo, int high, size_t size);
+/**
+ * quick_sort - Function to swap to nodes
+ * @array: list to be swapped
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	quicksort(array, 0, size - 1, size);
+}
+/**
+ * partition - Function to swap to nodes
+ * @array: list to be swapped
+ * @lo: low index
+ * @high: low index
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+int partition(int *array, int lo, int high, size_t size)
+{
+	int pivot = 0, i = 0, j = 0, temp = 0;
+
+	(void)size;
+	pivot = array[high];
+	i = lo;
+	for (j = lo; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			i++;
+		}
+	}
+	temp = array[i];
+	array[i] = array[high];
+	array[high] = temp;
+	print_array(array, size);
+	return (i);
+}
+/**
+ * quicksort - Function to swap to nodes
+ * @array: list to be swapped
+ * @lo: low index
+ * @high: low index
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void quicksort(int *array, int lo, int high, size_t size)
+{
+	int p = 0;
+
+	if (lo < high)
+	{
+		p = partition(array, lo, high, size);
+		quicksort(array, lo, p - 1, size);
+		quicksort(array, p + 1, high, size);
+	}
+}
+
