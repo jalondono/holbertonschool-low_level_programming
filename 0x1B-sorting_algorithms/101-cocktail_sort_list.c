c21247c793fc36c045b3228f86aa533c12edde03
jalondono <juanlondono151776@hotmail.com>
2019-10-30 14:18:22 -0500

    upload task 101

---
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -0,0 +1,93 @@
+#include "sort.h"
+/**
+ * swapNode101 - Function to swap to nodes
+ * @copy: list to be swapped
+ *
+ * Return: Nothing
+ */
+void swapNode101(listint_t *copy)
+{
+	listint_t *nextAux;
+	listint_t *copyHead = copy;
+
+	if (copyHead->next == NULL)
+		return;
+	copyHead = copyHead->next;
+	nextAux = copyHead->next;
+	if (copyHead->next != NULL)
+		copyHead->next->prev = copy;
+	if (copy->prev != NULL)
+		copy->prev->next = copyHead;
+	copyHead->next = copy;
+	copyHead->prev = copy->prev;
+	copy->next = nextAux;
+	copy->prev = copyHead;
+}
+/**
+* cocktail_sort_list - Function to swap to nodes
+* @list: list to be swapped
+*
+* Return: Nothing
+*/
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *actList, *prevList, *min, *max;
+	int exit = 1, exitForward = 1, exitReverse = 1, firtsTime = 0;
+
+	if (list == NULL || *list == NULL)
+		return;
+	if ((*list)->next == NULL)
+		return;
+	actList = (*list), prevList = (*list), min = (*list);
+	while (exit == 1)
+	{
+		exit = 0;
+		while (exitForward == 1)
+		{
+			if (actList->n > actList->next->n)
+			{
+				swapNode101(actList);
+				if ((*list)->prev != NULL)
+					(*list) = (*list)->prev;
+				print_list(*list), exit = 1;
+			}
+			else
+				actList = actList->next;
+			if (firtsTime == 0)
+			{
+				if (actList->next == NULL)
+					exitForward = 0, exitReverse = 1, max = actList;
+			}
+			else
+			{
+				if (actList->next == max)
+					max = max->prev, exitForward = 0, exitReverse = 1;
+			}
+		}
+		prevList = max->prev;
+		while (exitReverse == 1)
+		{
+			if (prevList->prev == NULL)
+				return;
+			if (prevList->n < prevList->prev->n)
+			{
+				swapNode101(prevList->prev);
+				if ((*list)->prev != NULL)
+					(*list) = (*list)->prev;
+				print_list(*list), exit = 1;
+			}
+			else
+				prevList = prevList->prev;
+			if (firtsTime == 0)
+			{
+				if (prevList->prev == NULL)
+					exitReverse = 0, exitForward = 1, min = prevList;
+			}
+			else
+			{
+				if (prevList->prev == min)
+					exitReverse = 0, exitForward = 1, min = prevList;
+			}
+		}	actList = min->next, firtsTime++;
+	}
+}
