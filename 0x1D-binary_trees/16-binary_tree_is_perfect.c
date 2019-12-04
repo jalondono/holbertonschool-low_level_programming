f723deae170a095ba50943594560064e5346160b
jalondono <juanlondono151776@hotmail.com>
2019-12-04 16:34:00 -0500

    upload task 16

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -6,7 +6,7 @@
  * Return: Always 1
  * (Success)
  */
-size_t binary_tree_height_Aux1(const binary_tree_t *tree)
+size_t binary_tree_height_Aux2(const binary_tree_t *tree)
 {
 	int leftHeight = 0;
 	int rightHeight = 0;
@@ -14,9 +14,9 @@ size_t binary_tree_height_Aux1(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-		leftHeight = binary_tree_height_Aux1(tree->left);
+		leftHeight = binary_tree_height_Aux2(tree->left);
 	if (tree->right)
-		rightHeight = binary_tree_height_Aux1(tree->right);
+		rightHeight = binary_tree_height_Aux2(tree->right);
 	if (leftHeight > rightHeight)
 		return (leftHeight + 1);
 	else
@@ -29,13 +29,13 @@ size_t binary_tree_height_Aux1(const binary_tree_t *tree)
  * Return: Always 1
  * (Success)
  */
-size_t binary_tree_height1(const binary_tree_t *tree)
+size_t binary_tree_height2(const binary_tree_t *tree)
 {
 	unsigned int total = 0;
 
 	if (!tree)
 		return (0);
-	total = binary_tree_height_Aux1(tree);
+	total = binary_tree_height_Aux2(tree);
 	return (total);
 }
 /**
@@ -53,33 +53,9 @@ int binary_tree_balance1(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	leftHeight = binary_tree_height1(tree->left);
-	rightHeight = binary_tree_height1(tree->right);
+	leftHeight = binary_tree_height2(tree->left);
+	rightHeight = binary_tree_height2(tree->right);
 	return (leftHeight - rightHeight);
-}
-
-/**
- * binary_tree_is_perfect1 - Write a function that counts the leaves in
- * @tree: Where tree is a pointer to the root node of the tree to count the
- * number of nodes
- * The value in the node must be passed as a parameter to this function.
- * Return: Always 1
- * (Success)
- */
-
-int binary_tree_is_perfect1(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	if ((tree->left && tree->right) || (!tree->left && !tree->right))
-	{
-		binary_tree_is_perfect1(tree->left);
-		binary_tree_is_perfect1(tree->right);
-		return (1);
-	}
-	else
-		return (0);
 }
 
 /**
@@ -92,19 +68,16 @@ int binary_tree_is_perfect1(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int lfull = 0;
-	int rfull = 0;
-	int balanced = 0;
-
 	if (!tree)
 		return (0);
-	balanced = binary_tree_balance1(tree);
-	lfull = binary_tree_is_perfect1(tree->left);
-	rfull = binary_tree_is_perfect1(tree->right);
-	if (!tree->left && !tree->right)
-		return (1);
-	if (lfull == 1 && rfull == 1 && balanced == 0)
-		return (1);
+
+	if (binary_tree_is_full(tree) && binary_tree_balance1(tree) == 0)
+	{
+		if ((binary_tree_is_perfect(tree->left) + binary_tree_is_perfect(tree->right)) % 2 == 0)
+			return (1);
+		else
+			return (0);
+	}
 	else
 		return (0);
 }
