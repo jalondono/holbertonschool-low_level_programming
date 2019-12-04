a8a58807837e3964ec7f9849b9d88b2ba2a754dd
jalondono <juanlondono151776@hotmail.com>
2019-12-04 15:54:55 -0500

    fixed fifteen point

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,30 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_fullsearch - Write a function that counts the leaves in
- * @tree: Where tree is a pointer to the root node of the tree to count the
- * number of nodes
- * The value in the node must be passed as a parameter to this function.
- * Return: Always 1
- * (Success)
- */
-
-int binary_tree_is_fullsearch(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	if ((tree->left && tree->right) || (!tree->left && !tree->right))
-	{
-		binary_tree_is_fullsearch(tree->left);
-		binary_tree_is_fullsearch(tree->right);
-		return (1);
-	}
-	else
-		return (0);
-}
-
-/**
  * binary_tree_is_full - Write a function that counts the leaves in a binary
  * @tree: Where tree is a pointer to the root node of the tree to count the
  * number of nodes
@@ -39,11 +15,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	lfull = binary_tree_is_fullsearch(tree->left);
-	rfull = binary_tree_is_fullsearch(tree->right);
-
 	if (!tree->left && !tree->right)
 		return (1);
+
+	lfull = binary_tree_is_full(tree->left);
+	rfull = binary_tree_is_full(tree->right);
 
 	if (lfull == 1 && rfull == 1)
 		return (1);
