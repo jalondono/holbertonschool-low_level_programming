ee0baba0be82371dc841359bfc3f475d883e9661
jalondono <juanlondono151776@hotmail.com>
2019-12-04 16:35:38 -0500

    upload task 16

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,31 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_full1 - Write a function that counts the leaves in a binary
+ * @tree: Where tree is a pointer to the root node of the tree to count the
+ * number of nodes
+ * The value in the node must be passed as a parameter to this function.
+ * Return: Always 1
+ * (Success)
+ */
+int binary_tree_is_full1(const binary_tree_t *tree)
+{
+	int lfull = 0;
+	int rfull = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+
+	lfull = binary_tree_is_full1(tree->left);
+	rfull = binary_tree_is_full1(tree->right);
+
+	if (lfull == 1 && rfull == 1)
+		return (1);
+	else
+		return (0);
+}
+/**
  * binary_tree_height_Aux1- function that checks if a given node is a root
  * @tree: Where tree is a pointer to the root node of the tree to traverse
  * The value in the node must be passed as a parameter to this function.
