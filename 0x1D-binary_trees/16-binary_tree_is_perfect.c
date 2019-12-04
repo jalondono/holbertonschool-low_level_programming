84d2b25a879594d5512190c84d60def6bec08eae
jalondono <juanlondono151776@hotmail.com>
2019-12-04 15:30:44 -0500

    fixed betty 16 task

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,4 +1,62 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height_Aux1- function that checks if a given node is a root
+ * @tree: Where tree is a pointer to the root node of the tree to traverse
+ * The value in the node must be passed as a parameter to this function.
+ * Return: Always 1
+ * (Success)
+ */
+size_t binary_tree_height_Aux1(const binary_tree_t *tree)
+{
+	int leftHeight = 0;
+	int rightHeight = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		leftHeight = binary_tree_height_Aux1(tree->left);
+	if (tree->right)
+		rightHeight = binary_tree_height_Aux1(tree->right);
+	if (leftHeight > rightHeight)
+		return (leftHeight + 1);
+	else
+		return (rightHeight + 1);
+}
+/**
+ * binary_tree_height1- function that checks if a given node is a root
+ * @tree: Where tree is a pointer to the root node of the tree to traverse
+ * The value in the node must be passed as a parameter to this function.
+ * Return: Always 1
+ * (Success)
+ */
+size_t binary_tree_height1(const binary_tree_t *tree)
+{
+	unsigned int total = 0;
+
+	if (!tree)
+		return (0);
+	total = binary_tree_height_Aux1(tree);
+	return (total);
+}
+/**
+ * binary_tree_balance1- Write a function that counts the leaves in a binary
+ * @tree: Where tree is a pointer to the root node of the tree to count the
+ * number of nodes
+ * The value in the node must be passed as a parameter to this function.
+ * Return: Always 1
+ * (Success)
+ */
+int binary_tree_balance1(const binary_tree_t *tree)
+{
+	int leftHeight = 0;
+	int rightHeight = 0;
+
+	if (!tree)
+		return (0);
+	leftHeight = binary_tree_height1(tree->left);
+	rightHeight = binary_tree_height1(tree->right);
+	return (leftHeight - rightHeight);
+}
 
 /**
  * binary_tree_is_perfect1 - Write a function that counts the leaves in
@@ -40,7 +98,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	balanced = binary_tree_balance(tree);
+	balanced = binary_tree_balance1(tree);
 	lfull = binary_tree_is_perfect1(tree->left);
 	rfull = binary_tree_is_perfect1(tree->right);
 	if (!tree->left && !tree->right)
