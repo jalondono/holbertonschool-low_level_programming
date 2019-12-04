0d62bb00931d9d5616675b1c7b7ec1179453c64a
jalondono <juanlondono151776@hotmail.com>
2019-12-04 13:32:11 -0500

    fixed position of print

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -14,9 +14,9 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
+	func(tree->n);
 	if (tree->left)
 		binary_tree_preorder(tree->left, func);
-	func(tree->n);
 	if (tree->right)
 		binary_tree_preorder(tree->right, func);
 }
