55e9c3bd5a51f162c1b30a63e3386bd74826465c
jalondono <juanlondono151776@hotmail.com>
2020-01-15 14:45:36 -0500

    fixed betty

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,7 +21,7 @@ int jump_search(int *array, size_t size, int value)
 	high = step;
 	real_size = size - 1;
 	printf("Value checked array[%d] = [%d]\n", low, array[low]);
-	while(array[high] < value)
+	while (array[high] < value)
 	{
 		printf("Value checked array[%d] = [%d]\n", high, array[high]);
 		if (array[low] == value)
